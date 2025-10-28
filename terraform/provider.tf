15674252c832beec14a086deba2c398f0589ef03
Lamine-art-png <dabolamine811@gmail.com>
2025-10-28 16:37:15 -0700

    Refactor provider.tf for consistency and clarity

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,11 @@
 terraform {
   required_version = ">= 1.5.0"
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+    aws    = { source = "hashicorp/aws", version = "~> 5.0" }
     random = { source = "hashicorp/random", version = "~> 3.6" }
   }
-
   backend "s3" {
-    bucket         = "<YOUR-US-WEST-1-BUCKET>"   # EXACT bucket you created
+    bucket         = "<YOUR-US-WEST-1-BUCKET>"  # exact name from CloudShell
     key            = "manulife-pilot/terraform.tfstate"
     region         = "us-west-1"
     dynamodb_table = "tf-locks"
@@ -14,6 +13,5 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.region
-}
+provider "aws" { region = var.region }
+
