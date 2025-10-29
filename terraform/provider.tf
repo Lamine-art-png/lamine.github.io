fa3360b466c5d8f69628fc2334e8ee3ab021eaa9
Lamine-art-png <dabolamine811@gmail.com>
2025-10-29 13:16:13 -0700

    Update AWS provider configuration in provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,17 +1,19 @@
 terraform {
   required_version = ">= 1.5.0"
   required_providers {
-    aws    = { source = "hashicorp/aws", version = "~> 5.0" }
+    aws    = { source = "hashicorp/aws",    version = "~> 5.0" }
     random = { source = "hashicorp/random", version = "~> 3.6" }
   }
- terraform {
+}
+
+terraform {
   backend "s3" {
-    bucket         = "<EXACT_BUCKET_NAME_IN_US-WEST-1>"   # if you created NEW_BUCKET, put it here
+    bucket         = "agroai-tfstate-ld-usw1-1761768239"  
     key            = "manulife-pilot/terraform.tfstate"
     region         = "us-west-1"
     dynamodb_table = "tf-locks"
     encrypt        = true
   }
 }
-provider "aws" { region = var.region }
 
+provider "aws" { region = var.region }
