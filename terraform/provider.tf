6a773ab385446dbf27e3f3f376c73cf6ab63c39c
Lamine-art-png <dabolamine811@gmail.com>
2025-10-29 12:59:33 -0700

    Change S3 bucket name in Terraform configuration
    Updated S3 bucket name placeholder in provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,12 +5,13 @@ terraform {
     random = { source = "hashicorp/random", version = "~> 3.6" }
   }
   backend "s3" {
-    bucket         = "<agroai-tfstate-ld-20251028-ncal>"  # exact name from CloudShell
-    key            = "manulife-pilot/terraform.tfstate"
-    region         = "us-west-1"
-    dynamodb_table = "tf-locks"
-    encrypt        = true
-  }
+  bucket         = "<your-state-bucket-name>"   # EXACT name from step 1
+  key            = "manulife-pilot/terraform.tfstate"
+  region         = "us-west-1"
+  dynamodb_table = "tf-locks"
+  encrypt        = true
+}
+
 }
 
 provider "aws" { region = var.region }
