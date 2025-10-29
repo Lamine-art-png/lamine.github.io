c9a5bb6cdc2dba7588696b486d3f7ce537efeea9
Lamine-art-png <dabolamine811@gmail.com>
2025-10-29 13:06:15 -0700

    Update S3 bucket name in Terraform provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,15 +4,14 @@ terraform {
     aws    = { source = "hashicorp/aws", version = "~> 5.0" }
     random = { source = "hashicorp/random", version = "~> 3.6" }
   }
+ terraform {
   backend "s3" {
-  bucket         = "<your-state-bucket-name>"   # EXACT name from step 1
-  key            = "manulife-pilot/terraform.tfstate"
-  region         = "us-west-1"
-  dynamodb_table = "tf-locks"
-  encrypt        = true
+    bucket         = "<EXACT_BUCKET_NAME_IN_US-WEST-1>"   # if you created NEW_BUCKET, put it here
+    key            = "manulife-pilot/terraform.tfstate"
+    region         = "us-west-1"
+    dynamodb_table = "tf-locks"
+    encrypt        = true
+  }
 }
-
-}
-
 provider "aws" { region = var.region }
 
