23aafb38a3eb22cf5e8c4b2999e47a9b2224a5aa
Lamine-art-png <dabolamine811@gmail.com>
2025-10-31 09:02:28 -0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,25 @@
-variable "region"        { type = string  default = "us-west-1" }
-variable "project"       { type = string  default = "agroai-manulife-pilot" }
-variable "env"           { type = string  default = "dev" }
-variable "vpc_cidr"      { type = string  default = "10.42.0.0/16" }
-variable "desired_count" { type = number  default = 1 }
+# terraform/variables.tf
+variable "region" {
+  type    = string
+  default = "us-west-1"
+}
 
+variable "project" {
+  type    = string
+  default = "agroai-manulife-pilot"
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.42.0.0/16"
+}
+
+variable "desired_count" {
+  type    = number
+  default = 1
+}
