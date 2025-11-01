14d8d20ce8819d91c707b858921f523f6dc91120
Lamine-art-png <dabolamine811@gmail.com>
2025-11-01 13:18:14 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,18 @@
 #############################################
 # terraform/main.tf  (us-west-1 pilot)
+# - VPC (2 public + 2 private subnets)
+# - SG allowing HTTP
+# - CloudWatch log group
+# - ECS cluster + Fargate service
+# - Public IP on tasks, image = public NGINX
 #############################################
 
+# Availability Zones
 data "aws_availability_zones" "available" {
   state = "available"
 }
 
+# --- Networking (VPC/Subnets) ---
 module "network" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
@@ -21,6 +28,7 @@ module "network" {
   single_nat_gateway = true
 }
 
+# --- Security Group (allow HTTP from Internet) ---
 resource "aws_security_group" "api_sg" {
   name        = "${var.project}-api-sg"
   description = "Allow HTTP from internet"
@@ -41,11 +49,13 @@ resource "aws_security_group" "api_sg" {
   }
 }
 
+# --- CloudWatch Log Group for ECS ---
 resource "aws_cloudwatch_log_group" "api" {
   name              = "/ecs/${var.project}-api"
   retention_in_days = 14
 }
 
+# --- ECS Cluster ---
 module "ecs" {
   source  = "terraform-aws-modules/ecs/aws"
   version = "~> 5.11"
@@ -54,29 +64,38 @@ module "ecs" {
   fargate_capacity_providers = ["FARGATE"]
 }
 
+# --- ECS Fargate Service (public, no ALB) ---
 module "api_service" {
   source  = "terraform-aws-modules/ecs/aws//modules/service"
   version = "~> 5.11"
 
-  name            = "${var.project}-api"
-  cluster_arn     = module.ecs.cluster_arn
-  cpu             = 256
-  memory          = 512
-  desired_count   = var.desired_count
-  launch_type     = "FARGATE"
+  name          = "${var.project}-api"
+  cluster_arn   = module.ecs.cluster_arn
+  cpu           = 256
+  memory        = 512
+  desired_count = var.desired_count
+  launch_type   = "FARGATE"
+
+  # Fast pilot: run in public subnets with a public IP
   subnet_ids         = module.network.public_subnets
   security_group_ids = [aws_security_group.api_sg.id]
   assign_public_ip   = true
   force_new_deployment = true
+  enable_execute_command = true
 
+  # Use a public image so we don't depend on ECR yet
   container_definitions = [
     {
       name      = "api"
       image     = "public.ecr.aws/nginx/nginx:latest"
       essential = true
-      portMappings = [{ containerPort = 80, hostPort = 80 }]
+
+      portMappings = [
+        { containerPort = 80, hostPort = 80 }
+      ]
+
       logConfiguration = {
-        logDriver = "awslogs",
+        logDriver = "awslogs"
         options = {
           awslogs-group         = aws_cloudwatch_log_group.api.name
           awslogs-region        = var.region
@@ -85,4 +104,6 @@ module "api_service" {
       }
     }
   ]
+
+  depends_on = [aws_cloudwatch_log_group.api]
 }
