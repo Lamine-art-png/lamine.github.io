f4d11f7f18c2a72218ad2aad5017b58818ec6c9d
Lamine-art-png <dabolamine811@gmail.com>
2025-11-02 12:18:54 -0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,11 +8,6 @@ variable "region" {
   default = "us-west-1"
 }
 
-variable "vpc_cidr" {
-  type    = string
-  default = "10.42.0.0/16"
-}
-
 variable "desired_count" {
   type    = number
   default = 1
