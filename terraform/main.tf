0f8eca8605f5d2be452c694392c2f4e7311efe4b
Lamine-art-png <dabolamine811@gmail.com>
2025-11-02 19:07:11 -0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,35 @@
- #############################################
-# ECS Fargate service in the default VPC
+#############################################
+# terraform/main.tf  (us-west-1 pilot, Fargate)
 #############################################
 
-# Use the account’s default VPC and its default subnets
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default_vpc" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-  filter {
-    name   = "default-for-az"
-    values = ["true"]
-  }
-}
-
 locals {
-  subnets        = slice(data.aws_subnets.default_vpc.ids, 0, 2)
-  log_group_name = "/aws/ecs/${var.project}-api"
+  # unique per run (CI overrides service_name); avoids log group collisions
+  log_group_name = "/aws/ecs/${var.service_name}/api"
 }
 
-# Security group (unique name via prefix)
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "network" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.21"
+
+  name = "${var.project}-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  private_subnets = ["10.42.1.0/24", "10.42.2.0/24"]
+  public_subnets  = ["10.42.101.0/24", "10.42.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
 resource "aws_security_group" "api_sg" {
-  name_prefix = "${var.project}-api-sg-"
+  name        = "${var.project}-api-sg"
   description = "Allow HTTP from internet"
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = module.network.vpc_id
 
   ingress {
     from_port   = 80
@@ -42,18 +44,15 @@ resource "aws_security_group" "api_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = { Name = "${var.project}-api-sg" }
 }
 
-# ECS cluster (do NOT create a cluster CW log group)
 module "ecs" {
   source  = "terraform-aws-modules/ecs/aws"
   version = "~> 5.12"
 
-  cluster_name                = "${var.project}-cluster"
-  create_cloudwatch_log_group = false
+  cluster_name = "${var.project}-cluster"
 
+  # must be a MAP (not a list)
   fargate_capacity_providers = {
     FARGATE = {
       default_capacity_provider_strategy = [{
@@ -64,12 +63,11 @@ module "ecs" {
   }
 }
 
-# Fargate service
 module "api_service" {
   source  = "terraform-aws-modules/ecs/aws//modules/service"
   version = "~> 5.12"
 
-  name = var.service_name
+  name                   = var.service_name
   cluster_arn            = module.ecs.cluster_arn
   desired_count          = var.desired_count
   launch_type            = "FARGATE"
@@ -78,11 +76,11 @@ module "api_service" {
   assign_public_ip       = true
   enable_execute_command = true
   force_new_deployment   = true
-  platform_version       = "1.4.0"
 
-  subnet_ids         = local.subnets
+  subnet_ids         = module.network.public_subnets
   security_group_ids = [aws_security_group.api_sg.id]
 
+  # One container (nginx), kept in foreground so ECS sees it as healthy
   container_definitions = {
     api = {
       image     = "public.ecr.aws/nginx/nginx:stable"
