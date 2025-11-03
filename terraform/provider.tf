b8a870fcce97d04dd2a589cff92bd1053e964de9
Lamine-art-png <dabolamine811@gmail.com>
2025-11-02 19:05:08 -0800

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = var.region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "agroai-tfstate-ld-usw1-1761768239"  # your bucket
+    key            = "state/lamine-github-io.tfstate"
+    region         = "us-west-1"
+    dynamodb_table = "tf-locks"
+    encrypt        = true
+  }
+}
