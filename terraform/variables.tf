0fa837ad219ca91c26b5663506ec94d496e4f660
Lamine-art-png <dabolamine811@gmail.com>
2025-11-02 19:26:31 -0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,20 @@
-variable "region"        { type = string  default = "us-west-1" }
-variable "project"       { type = string  default = "agroai-manulife-pilot" }
-variable "desired_count" { type = number  default = 1 }
-variable "vpc_cidr"      { type = string  default = "10.42.0.0/16" }
+variable "region" {
+  type    = string
+  default = "us-west-1"
+}
 
-# Will be overridden by CI so every run uses a unique ECS service/log group name
-variable "service_name"  { type = string  default = "agroai-manulife-pilot-api-v2" }
+variable "project" {
+  type    = string
+  default = "agroai-manulife-pilot"
+}
+
+variable "desired_count" {
+  type    = number
+  default = 1
+}
+
+/* Optional. CI will pass a unique value to avoid name/log-group collisions. */
+variable "service_name" {
+  type    = string
+  default = ""  # when empty, we'll derive from var.project
+}
