1b177a92288bb63583322659e506c7bd1886c254
Lamine-art-png <dabolamine811@gmail.com>
2025-11-03 15:28:25 -0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,11 +11,13 @@ variable "aws_region" {
 
 variable "container_image" {
   type    = string
-  # Keep nginx for now; replace with your ECR URI when ready
-  default = "public.ecr.aws/nginx/nginx:stable-alpine"
+  # point to your ECR image (must exist and be public to the ECS task via the exec role)
+  default = "292039821285.dkr.ecr.us-west-1.amazonaws.com/agroai-manulife-pilot-api"
+  # e.g. "292039821285.dkr.ecr.us-west-1.amazonaws.com/agroai-manulife-pilot-api:latest"
 }
 
 variable "health_check_path" {
   type    = string
   default = "/"
 }
+
