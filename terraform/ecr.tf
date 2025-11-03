1c56e261acb1ec665b957adeb09486d33b668521
Lamine-art-png <dabolamine811@gmail.com>
2025-11-03 14:36:26 -0800

    Update ecr.tf

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,10 +1,8 @@
-resource "aws_ecr_repository" "api" {
-  name                 = "${var.project}-api"
-  image_scanning_configuration { scan_on_push = true }
-  force_delete         = true
-  tags = { Project = var.project, ManagedBy = "terraform" }
+# Use the repo if it already exists
+data "aws_ecr_repository" "api" {
+  name = "${var.project}-api"
 }
 
 output "ecr_repo_url" {
-  value = aws_ecr_repository.api.repository_url
+  value = data.aws_ecr_repository.api.repository_url
 }
