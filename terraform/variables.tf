abbc4d2739edd0ae01ef10bc3e1d810838e9df65
Lamine-art-png <dabolamine811@gmail.com>
2025-11-03 12:01:23 -0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,15 @@
-variable "region" {
+variable "project" {
+  description = "Name/prefix for tags and resources"
+  type        = string
+  default     = "agroai-manulife-pilot"
+}
+
+variable "aws_region" {
   type    = string
   default = "us-west-1"
 }
 
-variable "project" {
+variable "aws_profile" {
   type    = string
-  default = "agroai-manulife-pilot"
-}
-
-variable "desired_count" {
-  type    = number
-  default = 1
-}
-
-# CI will override this with TF_VAR_service_name; default is fine locally
-variable "service_name" {
-  type    = string
-  default = "agroai-manulife-pilot-api"
+  default = null
 }
