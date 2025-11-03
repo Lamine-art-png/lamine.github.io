e846cde7dec0a004cf900257435b288a87b3ce7d
Lamine-art-png <dabolamine811@gmail.com>
2025-11-03 13:36:43 -0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,14 @@ variable "aws_region" {
   type    = string
   default = "us-west-1"
 }
+
+variable "container_image" {
+  type    = string
+  # Keep nginx for now; replace with your ECR URI when ready
+  default = "public.ecr.aws/nginx/nginx:stable-alpine"
+}
+
+variable "health_check_path" {
+  type    = string
+  default = "/"
+}
