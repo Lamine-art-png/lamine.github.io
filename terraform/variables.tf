0931b824ab47a69d9b8def267e541744699a0dd0
Lamine-art-png <dabolamine811@gmail.com>
2025-11-03 11:48:14 -0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,13 +8,13 @@ variable "project" {
   default = "agroai-manulife-pilot"
 }
 
-# Optional: CI can override with TF_VAR_service_name
-variable "service_name" {
-  type    = string
-  default = ""
-}
-
 variable "desired_count" {
   type    = number
   default = 1
+}
+
+# CI will override this with TF_VAR_service_name; default is fine locally
+variable "service_name" {
+  type    = string
+  default = "agroai-manulife-pilot-api"
 }
