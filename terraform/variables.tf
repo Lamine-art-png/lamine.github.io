50e2df494953d7a8943a4b8c66fdfd88de061e19
Lamine-art-png <dabolamine811@gmail.com>
2025-11-04 13:59:19 -0800

    Refactor variable descriptions in variables.tf
    Updated variable descriptions and default values.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,14 +9,14 @@ variable "aws_region" {
   default = "us-west-1"
 }
 
-# IMPORTANT: repository URL ONLY (no :tag). The workflows use :latest.
+# ECR repository URL WITHOUT a tag. We'll append :latest in main.tf
 variable "container_image" {
-  type    = string
-  default = "292039821285.dkr.ecr.us-west-1.amazonaws.com/agroai-manulife-pilot-api"
+  description = "ECR repo URL (no tag)"
+  type        = string
+  default     = "292039821285.dkr.ecr.us-west-1.amazonaws.com/agroai-manulife-pilot-api"
 }
 
-# Match the app's /health endpoint
 variable "health_check_path" {
   type    = string
-  default = "/health"
+  default = "/"
 }
