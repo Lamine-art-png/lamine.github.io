50138e169f42b9f396d0e906efb81b1f8e20c12f
Lamine-art-png <dabolamine811@gmail.com>
2025-11-04 15:12:37 -0800

    Remove variable definitions from versions.tf
    Removed variable definitions for project, AWS region, and health check path.

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,17 +1,9 @@
-variable "project" {
-  description = "Name/prefix for tags and resources"
-  type        = string
-  default     = "agroai-manulife-pilot"
-}
-
-variable "aws_region" {
-  description = "AWS region to deploy into"
-  type        = string
-  default     = "us-west-1"
-}
-
-variable "health_check_path" {
-  description = "Path the container must return 200 on"
-  type        = string
-  default     = "/health"
+terraform {
+  required_version = "~> 1.9.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.60"
+    }
+  }
 }
