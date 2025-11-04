78eab5c70ecd998b44c97c55f632ca8ad24eecb6
Lamine-art-png <dabolamine811@gmail.com>
2025-11-04 11:06:20 -0800

    Refactor variable declarations in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,4 @@
-variable "project" {
-  description = "Name/prefix for tags and resources"
-  type        = string
-  default     = "agroai-manulife-pilot"
-}
-
-variable "aws_region" {
-  type    = string
-  default = "us-west-1"
-}
-
-variable "container_image" {
-  type    = string
-  default = "292039821285.dkr.ecr.us-west-1.amazonaws.com/agroai-manulife-pilot-api:latest"
-}
-
-variable "health_check_path" {
-  type    = string
-  default = "/health"
-}
+variable "project"       { type = string default = "agroai-manulife-pilot" }
+variable "aws_region"    { type = string default = "us-west-1" }
+variable "container_image" { type = string default = "292039821285.dkr.ecr.us-west-1.amazonaws.com/agroai-manulife-pilot-api" }
+variable "health_check_path" { type = string default = "/" }
