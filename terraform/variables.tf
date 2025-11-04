5f7f6328b8e68e8c061f2b6ae5af5a885efdd9a2
Lamine-art-png <dabolamine811@gmail.com>
2025-11-04 11:20:35 -0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,21 @@
-variable "project"       { type = string default = "agroai-manulife-pilot" }
-variable "aws_region"    { type = string default = "us-west-1" }
-variable "container_image" { type = string default = "292039821285.dkr.ecr.us-west-1.amazonaws.com/agroai-manulife-pilot-api" }
-variable "health_check_path" { type = string default = "/" }
+variable "project" {
+  description = "Name/prefix for tags and resources"
+  type        = string
+  default     = "agroai-manulife-pilot"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-west-1"
+}
+
+# IMPORTANT: repository URL only (no tag). main.tf appends :latest.
+variable "container_image" {
+  type    = string
+  default = "292039821285.dkr.ecr.us-west-1.amazonaws.com/agroai-manulife-pilot-api"
+}
+
+variable "health_check_path" {
+  type    = string
+  default = "/"
+}
