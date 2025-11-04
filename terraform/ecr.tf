bfe169cf176e422bdc452d3452d125f4b3f51dfc
Lamine-art-png <dabolamine811@gmail.com>
2025-11-04 15:11:45 -0800

    Add output for ECR repository URL
    Added output for ECR repository URL for debugging.

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,8 +1,8 @@
-# Use the repo if it already exists
 data "aws_ecr_repository" "api" {
   name = "${var.project}-api"
 }
 
+# optional, useful for debugging
 output "ecr_repo_url" {
   value = data.aws_ecr_repository.api.repository_url
 }
