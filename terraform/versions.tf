46b50954391ccbd37d3ed281cfa87ad84b586573
Lamine-art-png <dabolamine811@gmail.com>
2025-11-04 13:58:30 -0800

    Update AWS provider version and configure backend
    Updated AWS provider version constraint and added backend configuration.

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -3,7 +3,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 5.0"
     }
+  }
+
+  # ⬇⬇ Keep these values the same as the backend you already use
+  backend "s3" {
+    bucket         = "agroai-tfstate-ld-usw1-1761768239"   # <-- your bucket
+    key            = "manulife-pilot/terraform.tfstate"     # <-- your key
+    region         = "us-west-1"
+    dynamodb_table = "agroai-tf-locks"                      # <-- your lock table
+    encrypt        = true
   }
 }
