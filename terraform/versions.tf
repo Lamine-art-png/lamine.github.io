6d5b6e0d1128eda66b4a390af6d7ca54c0ac9475
Lamine-art-png <dabolamine811@gmail.com>
2025-11-04 15:02:37 -0800

    Update Terraform version requirements

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,18 +1,11 @@
+# versions.tf
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = "~> 1.9.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 5.60"
     }
-  }
-
-  # ⬇⬇ Keep these values the same as the backend you already use
-  backend "s3" {
-    bucket         = "agroai-tfstate-ld-usw1-1761768239"   # <-- your bucket
-    key            = "manulife-pilot/terraform.tfstate"     # <-- your key
-    region         = "us-west-1"
-    dynamodb_table = "agroai-tf-locks"                      # <-- your lock table
-    encrypt        = true
   }
 }
