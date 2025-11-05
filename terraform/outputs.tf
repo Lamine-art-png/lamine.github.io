59b370218a242ed78b1d0b410fd02d2d6ed18f23
Lamine-art-png <dabolamine811@gmail.com>
2025-11-04 19:35:53 -0800

    Update outputs in terraform/outputs.tf
    Replaced task_definition output with task_family and added log_group_name output.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "cluster_name"      { value = aws_ecs_cluster.pilot.name }
-output "service_name"      { value = aws_ecs_service.svc.name }
-output "task_definition"   { value = aws_ecs_task_definition.app.arn }
-output "security_group_id" { value = aws_security_group.ecs_tasks.id }
+output "cluster_name"   { value = aws_ecs_cluster.pilot.name }
+output "service_name"   { value = aws_ecs_service.svc.name }
+output "task_family"    { value = aws_ecs_task_definition.app.family }
+output "log_group_name" { value = aws_cloudwatch_log_group.ecs.name }
