34c6a475fe7cf76c2ab754b52a69dad0605dd79d
Lamine-art-png <dabolamine811@gmail.com>
2025-11-05 15:59:10 -0800

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "agroai-tfstate-ld-usw1-1761768239"
-    key            = "manulife-pilot/terraform.tfstate"
-    region         = "us-west-1"
-    dynamodb_table = "agroai-tf-locks"
-    encrypt        = true
+    bucket = "YOUR_STATE_BUCKET"
+    key    = "agroai-manulife/usw2/terraform.tfstate"   # <- new path
+    region = "us-west-2"
   }
 }
