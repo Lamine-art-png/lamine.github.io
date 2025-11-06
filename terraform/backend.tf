e0ba07c2baa2f20838bbd7b47bf9710588010980
Lamine-art-png <dabolamine811@gmail.com>
2025-11-06 07:28:06 -0800

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "agroai-tfstate-ld-usw1-1761768239"
-    key            = "lamine.github.io/terraform.tfstate"
-    region         = "us-west-1"
-    encrypt        = true
-    # dynamodb_table = "tf-locks-agroai"  # add only if you created this table
+    bucket = "agroai-tfstate-ld-usw1-1761768239" # use one of your existing us-west-1 buckets
+    key    = "lamine.github.io/terraform.tfstate"
+    region = "us-west-1"
+    encrypt = true
   }
 }
