003e5edab91b14558ff2e44100459555a38bfe47
Lamine-art-png <dabolamine811@gmail.com>
2025-11-06 07:53:31 -0800

    Delete terraform/ecr.tf

--- terraform/ecr.tf
+++
@@ -1,8 +0,0 @@
-# ecr.tf (keep only this one)
-data "aws_ecr_repository" "api" {
-  # pick ONE naming scheme and stick to it:
-  # Option A (explicit name):
-  name = "agroai-manulife-pilot-api"
-  # Option B (derived name):
-  # name = "${var.project}-api"
-}
