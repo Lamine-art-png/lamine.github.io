2ce9594daa5a601442aceb98fc347c411eee96bc
Lamine-art-png <dabolamine811@gmail.com>
2025-11-06 07:49:25 -0800

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "agroai-tfstate-ld-usw1-1761768239" # use one of your existing us-west-1 buckets
-    key    = "lamine.github.io/terraform.tfstate"
-    region = "us-west-1"
+    bucket  = "agroai-tfstate-ld-usw1-1761768239" # existing bucket in us-west-1
+    key     = "lamine.github.io/terraform.tfstate"
+    region  = "us-west-1"
     encrypt = true
   }
 }
