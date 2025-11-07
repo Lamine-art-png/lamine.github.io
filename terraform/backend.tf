568a20911b6692f5d3a3be02f6ebe50fb0862fa7
Lamine-art-png <dabolamine811@gmail.com>
2025-11-07 11:43:47 -0800

    Delete terraform/backend.tf

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "agroai-tfstate-ld-usw1-1761768239" # existing bucket in us-west-1
-    key     = "lamine.github.io/terraform.tfstate"
-    region  = "us-west-1"
-    encrypt = true
-  }
-}
