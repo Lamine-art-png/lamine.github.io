5b6968b87d41900e19ee9b6eda097b24e2a0917d
Lamine-art-png <dabolamine811@gmail.com>
2025-11-07 14:04:55 -0800

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "alb_dns_name" {
-  description = "ALB DNS name (if created)"
-  value       = var.create_alb && length(aws_lb.api) > 0 ? aws_lb.api[0].dns_name : null
+  description = "DNS name of the API ALB"
+  value       = aws_lb.api.dns_name
 }
 
 output "api_url" {
-  description = "Public URL if behind ALB (currently empty because create_alb = false)"
-  value       = var.create_alb && length(aws_lb.api) > 0 ? "http://${aws_lb.api[0].dns_name}" : ""
+  description = "Base URL for the API"
+  value       = "https://${aws_lb.api.dns_name}"
 }
