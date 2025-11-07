4f27452e161757b396682b758614bba665abd5f6
Lamine-art-png <dabolamine811@gmail.com>
2025-11-07 12:05:12 -0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,31 @@
 variable "project" {
-  type    = string
-  default = "agroai-manulife-pilot"
+  type        = string
+  default     = "agroai-manulife-pilot"
 }
 
 variable "aws_region" {
-  type    = string
-  default = "us-west-1"
+  type        = string
+  default     = "us-west-1"
 }
 
 variable "container_port" {
-  type    = number
-  default = 8000
+  type        = number
+  default     = 8000
 }
 
 variable "health_check_path" {
-  type    = string
-  default = "/"
-}
-
-variable "create_alb" {
-  description = "If true, create ALB + listeners + SG and (later) wire ECS service to it."
-  type        = bool
-  default     = false  # keep false until AWS lifts your ELB restriction
-}
-
-variable "api_image" {
-  description = "ECR image:tag for the API. If empty, ':latest' in the repo is used."
   type        = string
-  default     = ""
+  default     = "/v1/health"
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
 }
