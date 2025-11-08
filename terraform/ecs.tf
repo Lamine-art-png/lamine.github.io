ba9da2c9dd413c9a232c52d97ecdda3ef33c7460
Lamine-art-png <dabolamine811@gmail.com>
2025-11-08 07:37:57 -0800

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,16 +1,19 @@
+data "aws_ecr_repository" "api" {
+  name = "agroai-manulife-pilot-api"
+}
+
 resource "aws_ecs_cluster" "api" {
-  name = "agroai-manulife-pilot-cluster"
+  name = "${var.project}-cluster"
 }
 
 resource "aws_ecs_task_definition" "api" {
-  family                   = "agroai-manulife-pilot-api"
+  family                   = "${var.project}-api"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
 
-  # If you already defined this role elsewhere, keep only the reference.
-  # If not, you'll need to add the ecs_task_execution role in another file.
+  # If you have this role defined elsewhere; if not, comment this line for now.
   # execution_role_arn = aws_iam_role.ecs_task_execution.arn
 
   container_definitions = jsonencode([
@@ -21,7 +24,7 @@ resource "aws_ecs_task_definition" "api" {
 
       portMappings = [
         {
-          containerPort = 8000
+          containerPort = var.container_port
           protocol      = "tcp"
         }
       ]
@@ -30,7 +33,7 @@ resource "aws_ecs_task_definition" "api" {
 }
 
 resource "aws_ecs_service" "api" {
-  name            = "agroai-manulife-pilot-svc"
+  name            = "${var.project}-svc"
   cluster         = aws_ecs_cluster.api.id
   task_definition = aws_ecs_task_definition.api.arn
   desired_count   = 1
@@ -45,7 +48,7 @@ resource "aws_ecs_service" "api" {
   load_balancer {
     target_group_arn = aws_lb_target_group.api.arn
     container_name   = "api"
-    container_port   = 8000
+    container_port   = var.container_port
   }
 
   depends_on = [aws_lb_listener.api_https]
