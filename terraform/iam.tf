a52a3d6043e0f6afc563b56439e045d3206e9923
Lamine-art-png <dabolamine811@gmail.com>
2025-11-08 10:31:25 -0800

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,15 +3,13 @@ resource "aws_iam_role" "ecs_task_execution" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
       }
-    ]
+      Action = "sts:AssumeRole"
+    }]
   })
 }
 
