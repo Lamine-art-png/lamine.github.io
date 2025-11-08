7db5cfe03cf51b1f49d6f8bcdf2f47defe24b4a3
Lamine-art-png <dabolamine811@gmail.com>
2025-11-08 08:26:21 -0800

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,3 +1,7 @@
+resource "aws_ecs_cluster" "api" {
+  name = "agroai-manulife-pilot-cluster"
+}
+
 resource "aws_ecs_task_definition" "api" {
   family                   = "agroai-manulife-pilot-api"
   requires_compatibilities = ["FARGATE"]
@@ -19,4 +23,26 @@ resource "aws_ecs_task_definition" "api" {
       ]
     }
   ])
+}
+
+resource "aws_ecs_service" "api" {
+  name            = "agroai-manulife-pilot-svc"
+  cluster         = aws_ecs_cluster.api.id
+  task_definition = aws_ecs_task_definition.api.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = var.private_subnet_ids
+    security_groups  = [aws_security_group.ecs_api.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.api.arn
+    container_name   = "api"
+    container_port   = 8000
+  }
+
+  depends_on = [aws_lb_listener.api_https]
 }
