90a6fe08749aa5eca783e61b5e6e8b418249c328
Lamine-art-png <dabolamine811@gmail.com>
2025-11-08 08:19:24 -0800

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,13 +1,5 @@
-data "aws_ecr_repository" "api" {
-  name = "agroai-manulife-pilot-api"
-}
-
-resource "aws_ecs_cluster" "api" {
-  name = "${var.project}-cluster"
-}
-
 resource "aws_ecs_task_definition" "api" {
-  family                   = "${var.project}-api"
+  family                   = "agroai-manulife-pilot-api"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = "256"
@@ -16,37 +8,15 @@ resource "aws_ecs_task_definition" "api" {
   container_definitions = jsonencode([
     {
       name      = "api"
-      image     = var.api_image  # from terraform.tfvars
+      image     = "292039821285.dkr.ecr.us-west-1.amazonaws.com/agroai-manulife-pilot-api:latest"
       essential = true
 
       portMappings = [
         {
-          containerPort = var.container_port
+          containerPort = 8000
           protocol      = "tcp"
         }
       ]
     }
   ])
-}
-
-resource "aws_ecs_service" "api" {
-  name            = "${var.project}-svc"
-  cluster         = aws_ecs_cluster.api.id
-  task_definition = aws_ecs_task_definition.api.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets          = var.private_subnet_ids
-    security_groups  = [aws_security_group.ecs_api.id]
-    assign_public_ip = false
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.api.arn
-    container_name   = "api"
-    container_port   = var.container_port
-  }
-
-  depends_on = [aws_lb_listener.api_https]
 }
