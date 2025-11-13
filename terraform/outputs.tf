ad986b6e34c72317805b85ebe56022881f15862c
Lamine-art-png <dabolamine811@gmail.com>
2025-11-12 17:03:52 -0800

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "alb_dns_name" {
   description = "DNS name of the API ALB"
-  value       = aws_lb.api.dns_name
+  value       = "api-agroai-pilot-alb-tf-746939467.us-west-1.elb.amazonaws.com"
 }
 
 output "api_url" {
   description = "Base URL for the API"
-  value       = "https://${aws_lb.api.dns_name}"
+  value       = "https://api-agroai-pilot-alb-tf-746939467.us-west-1.elb.amazonaws.com"
 }
