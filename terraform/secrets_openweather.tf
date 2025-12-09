7636af993c57754571183fb6fd940558bc3a7ce8
Lamine-art-png <dabolamine811@gmail.com>
2025-12-08 19:31:37 -0800

    Add AWS Secrets Manager configuration for OpenWeather

---
+++ terraform/secrets_openweather.tf
@@ -0,0 +1,8 @@
+resource "aws_secretsmanager_secret" "openweather" {
+  name = "agroai/openweather_api_key"
+}
+
+resource "aws_secretsmanager_secret_version" "openweather" {
+  secret_id     = aws_secretsmanager_secret.openweather.id
+  secret_string = var.openweather_api_key
+}
