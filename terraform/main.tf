478aa04aa047efd66c0208a8f4a6d0bd966fb2d7
Lamine-art-png <dabolamine811@gmail.com>
2026-07-05 10:20:01 -0700

    Add deterministic runtime dependency providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,14 +6,17 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
   }
 
   backend "s3" {
     bucket         = "agroai-tfstate-ld-usw1-1761768239"
     key            = "agroai-pilot/us-west-1/terraform.tfstate"
     region         = "us-west-1"
-    dynamodb_table = "agroai-tf-locks" # 👈 use an existing table
+    dynamodb_table = "agroai-tf-locks"
     encrypt        = true
   }
 }
-
